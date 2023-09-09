73b78b7f19289b60ba6bf59b1f2127366f67156f
nazsph <a.nazsipahi@gmail.com>
2023-09-09 15:59:42 +0300

    test4

---
+++ app/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+
+export const Home = () => {
+  return (
+    <>
+        <View>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+      </>
+  )
+}
+
+export default Home
---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+import {Home} from '../app/Home'
+
+    const Main = () => {
+    return (
+      <>
+        <View>
+            <Text>
+                <Home/>
+            </Text>
+        </View>
+      </>
+    );
+  }
+export default Main
